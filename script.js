b8093cebc3cc664eb9861c87041ef27eb86240b2
marcos <marcosdelfino11235@gmail.com>
2021-12-01 15:19:57 -0300

    finished basic rps, will be updating with an interface later on!

--- script.js
+++ script.js
@@ -22,30 +22,87 @@ getRandomNum();
 // gets user play
 
 
-
-
+let win = "Computer picked rock, so you win!!"
+let loose = "Computer picked rock, that means you loose!!"
+let tie = "Computer picked rock too, so it's a Tie!"
+let humanScore = 0;
+let computerScore = 0;
 
 // now the program
 function game(){
     
-    let userPlay = window.prompt("Make your computerPlay!");
+    let userPlay = window.prompt("Rock, Paper or Scissors??");
     
     userPlay = userPlay.toLowerCase();
     
     getRandomNum();
-    
-    if (userPlay === computerPlay){
-        console.log("It's a Tie!")
+
+        
+    if(computerPlay === "rock"){
+        if (userPlay === "rock"){
+            console.log(tie)
+        } 
+        else if (userPlay === "paper"){
+            console.log(win)
+            humanScore ++
+            }
+        else{
+                console.log(loose)
+                computerScore ++
+            }
         }
-    else if ()
+            else if(computerPlay === "paper"){
+                if (userPlay === "rock"){
+                    console.log(loose)
+                    computerScore ++
+                } 
+                else if (userPlay === "paper"){
+                    console.log(tie)
+                }
+                else{
+                    console.log(win)
+                    humanScore ++
+                }
+            }
+            else{
+                if (userPlay === "rock"){
+                    console.log(win)
+                    humanScore ++
+                } 
+                else if (userPlay === "paper"){
+                    console.log(loose)
+                    computerScore ++
+                }
+                else{
+                    console.log(tie)
+                }
+                
+            }
+            
+            
 }
 
+function masterCall(){
+    for (i=1; i <= 5; i++){
+        game();
+
+    }
+} 
+
+
+masterCall();
+
+if (computerScore > humanScore){
+    console.log ('You loose :( ' + computerScore + ' to ' + humanScore)
+    }
+    else if(computerScore > humanScore){
+        console.log ('You win :) ' + humanScore + ' to ' + computerScore)
+    }
+    else{
+        console.log ("It's a Tie!!")
+    }
 
 
 
 
 
-function gameoutcome(userPlay){
-    if (userPlay = "rock"){
-        }
-}
